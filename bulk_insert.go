8705b67edc81c55e1aa8c2dca6a595ec614f2d0c
Beck Qin <enginebeck@gmail.com>
2019-10-15 14:02:05 +0800

    added get name map from model

--- bulk_insert.go
+++ bulk_insert.go
@@ -3,10 +3,11 @@ package gormbulk
 import (
 	"errors"
 	"fmt"
-	"github.com/jinzhu/gorm"
 	"reflect"
 	"strings"
 	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 // Insert multiple records at once
@@ -15,22 +16,66 @@ import (
 //                  Embedding a large number of variables at once will raise an error beyond the limit of prepared statement.
 //                  Larger size will normally lead the better performance, but 2000 to 3000 is reasonable.
 // [excludeColumns] Columns you want to exclude from insert. You can omit if there is no column you want to exclude.
-func BulkInsert(db *gorm.DB, objects []interface{}, chunkSize int, excludeColumns ...string) error {
+
+type BulkInfo struct {
+	DB *gorm.DB
+	Objects []interface{}
+	ChunkSize int
+	ExcludeColumns []string
+	CustomNameMap map[string]string
+}
+
+func (info *BulkInfo)BulkInsert() error {
 	// Split records with specified size not to exceed Database parameter limit
-	for _, objSet := range splitObjects(objects, chunkSize) {
-		if err := insertObjSet(db, objSet, excludeColumns...); err != nil {
+	for _, objSet := range splitObjects(info.Objects, info.ChunkSize) {
+		if err := info.insertObjSet(objSet); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-func insertObjSet(db *gorm.DB, objects []interface{}, excludeColumns ...string) error {
+// ref: gorm model struct
+func parseTagSetting(tags reflect.StructTag) map[string]string {
+	setting := map[string]string{}
+	for _, str := range []string{tags.Get("sql"), tags.Get("gorm")} {
+		if str == "" {
+			continue
+		}
+		tags := strings.Split(str, ";")
+		for _, value := range tags {
+			v := strings.Split(value, ":")
+			k := strings.TrimSpace(strings.ToUpper(v[0]))
+			if len(v) >= 2 {
+				setting[k] = strings.Join(v[1:], ":")
+			} else {
+				setting[k] = k
+			}
+		}
+	}
+	return setting
+}
+
+func GetNameMapFromModel(model interface{}) map[string]string {
+	reflectTypes := reflect.ValueOf(model).Type()
+	mp := make(map[string]string)
+	for i := 0; i < reflectTypes.NumField(); i++ {
+		tagSettings := parseTagSetting(reflectTypes.Field(i).Tag)
+		if columnName, ok := tagSettings["COLUMN"]; ok {
+			mp[reflectTypes.Field(i).Name] = columnName
+		} else {
+			mp[reflectTypes.Field(i).Name] = gorm.ToColumnName(reflectTypes.Field(i).Name)
+		}
+	}
+	return mp
+}
+
+func (info *BulkInfo)insertObjSet(objects []interface{}) error {
 	if len(objects) == 0 {
 		return nil
 	}
 
-	firstAttrs, err := extractMapValue(objects[0], excludeColumns)
+	firstAttrs, err := extractMapValue(objects[0], info.ExcludeColumns)
 	if err != nil {
 		return err
 	}
@@ -38,18 +83,22 @@ func insertObjSet(db *gorm.DB, objects []interface{}, excludeColumns ...string) 
 	attrSize := len(firstAttrs)
 
 	// Scope to eventually run SQL
-	mainScope := db.NewScope(objects[0])
+	mainScope := info.DB.NewScope(objects[0])
 	// Store placeholders for embedding variables
 	placeholders := make([]string, 0, attrSize)
 
 	// Replace with database column name
 	dbColumns := make([]string, 0, attrSize)
 	for _, key := range sortedKeys(firstAttrs) {
-		dbColumns = append(dbColumns, gorm.ToColumnName(key))
+		columnName:=info.CustomNameMap[key]
+		if columnName==""{
+			columnName = gorm.ToColumnName(key)
+		}
+		dbColumns = append(dbColumns, columnName)
 	}
 
 	for _, obj := range objects {
-		objAttrs, err := extractMapValue(obj, excludeColumns)
+		objAttrs, err := extractMapValue(obj, info.ExcludeColumns)
 		if err != nil {
 			return err
 		}
@@ -59,7 +108,7 @@ func insertObjSet(db *gorm.DB, objects []interface{}, excludeColumns ...string) 
 			return errors.New("attribute sizes are inconsistent")
 		}
 
-		scope := db.NewScope(obj)
+		scope := info.DB.NewScope(obj)
 
 		// Append variables
 		variables := make([]string, 0, attrSize)
@@ -81,7 +130,7 @@ func insertObjSet(db *gorm.DB, objects []interface{}, excludeColumns ...string) 
 		strings.Join(placeholders, ", "),
 	))
 
-	return db.Exec(mainScope.SQL, mainScope.SQLVars...).Error
+	return info.DB.Exec(mainScope.SQL, mainScope.SQLVars...).Error
 }
 
 // Obtain columns and values required for insert from interface
